97f9229f71692e396d092b71ccac50a118476c7c
Agimir <william@mesaindigo.com>
2024-12-21 15:28:08 -0600

    zone planning

--- data/zones.lua
+++ data/zones.lua
@@ -1,9 +1,30 @@
 
--- script data for primary functions
--- i.e. ped placement, vehicle spawning, etc.
+-- do not mess with unless you know what you are doing
+-- or you are autistic as fuck like me
 
 return {
 
-    
+    ['zn_ls_1'] = {
+        label = "minet.lsco.south.port",
+        signal = { min = 3, max = 6 },
+        ports = {
+            model = "",
+            spawn = {
+                vec4(0,0,0,0),
+                vec4(0,0,0,0),
+                vec4(0,0,0,0),
+                vec4(0,0,0,0),
+                vec4(0,0,0,0)
+            }
+        },
+        zone = {
+            vec(0,0,0),
+            vec(0,0,0),
+            vec(0,0,0),
+            vec(0,0,0),
+            vec(0,0,0),
+            vec(0,0,0),
+        }
+    }
 
 }
